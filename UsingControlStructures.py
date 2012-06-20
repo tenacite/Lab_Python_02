56243305baeaeb61afb940cfef7a08e9701ad28c
Nii Logoligi <niilogoligi@niilogoligi-Satellite-C655.(none)>
2012-06-20 17:00:02 +0000

    i didn't understand the part II question 2

---
+++ UsingControlStructures.py
@@ -0,0 +1,28 @@
+theInput = raw_input("Enter an integer: ")
+i = theInput
+i = int(i)
+if i%2==0:
+    print 'even'
+else:
+    print 'odd'
+print '------------------------'
+primarySchoolAge= 5
+legalVotingAge= 18
+ageForPresidency= 45
+retirementAge=60
+personsAge=input("Enter an age: ")
+if personsAge<primarySchoolAge:
+    print "too young"
+elif personsAge<ageForPresidency:
+    print "You can't be president"
+elif personsAge>=legalVotingAge:
+    print "Remember to vote"
+elif personsAge>=retirementAge:
+    print "Too Old"
+elif personsAge>=ageForPresidency:
+    print "Vote for me"
+
+print"-----------------------------"
+for i in range(40,0,-1):
+    if i%3 == 0:
+         print i
